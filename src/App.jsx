dec3caa84c5ffb104bdd53269da0ed60bde35bfe
Emilio Filandini <emiliofilandini@gmail.com>
2024-05-30 19:06:19 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({ nombre: "Cargando", edad: "Cargando" }); //este guarda el usuario
+  const [count, setCount] = useState(0); //Este estado almacena el contador
+
+  useEffect(() => {
+    setTimeout(() => {
+      setUser({ nombre: "loading", edad: "loading" });
+      setUser({ nombre: "Emilio", edad: 25 });
+    }, 3000);
+    return setUser({ nombre: "Cargando", edad: "Cargando" });
+  }, [count]);
+
+  function increaseCount() {
+    setCount(count + 1);
+  }
+
+  function resetCount() {
+    setCount(0);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h2>Nombre : {user.nombre}</h2>
+      <h2>Edad: {user.edad}</h2>
+      <button onClick={increaseCount}>incrementar Contador</button>
+      <h4>{count}</h4>
+      <button onClick={resetCount}>Reiniciar Contador</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
