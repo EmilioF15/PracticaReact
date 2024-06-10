624e225fafce5d4001f22af04af21f92b784ab32
Emilio Filandini <emiliofilandini@gmail.com>
2024-06-10 18:30:35 -0300

    Creo clase5

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import "./App.css";
 import WithLoggin from "./components/Hocs/WithLoggin";
 import Form from "./components/Formulario";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 
 function App() {
-
   const FormWithLoggin = WithLoggin(Form);
 
   return (
-    <>
-    <h2>Pero la puta madre</h2>
-      <FormWithLoggin/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ItemListContainer/>}/>
+      </Routes>
+    </BrowserRouter>
   );
-
 }
-
 
 export default App;
