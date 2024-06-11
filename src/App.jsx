6b316410f51567747be868d24f48bf276a29ed9c
Emilio Filandini <emiliofilandini@gmail.com>
2024-06-11 00:26:32 -0300

    agrego cartProvider

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import { CartProvider } from "./context/cartContext";
 
 function App() {
   const FormWithLoggin = WithLoggin(Form);
 
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<ItemListContainer />} />
-        <Route path="/category/:id" element={<ItemListContainer />} />
-        <Route path="/item/:id" element={<ItemDetailContainer />} />
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/category/:id" element={<ItemListContainer />} />
+          <Route path="/item/:id" element={<ItemDetailContainer />} />
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
